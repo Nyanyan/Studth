efe9d3e850afa44725a8ea7b8aaa5982ffdee259
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-01-18 11:53:56 +0900

    start using cython

---
+++ exe_cython.py
@@ -0,0 +1,24 @@
+import sys
+import subprocess
+import os
+
+args = sys.argv
+
+if len(args) != 2:
+    print('args error exit')
+    exit()
+
+
+cmd = 'python setup_exe_cython.py build_ext --inplace'
+try:
+    o = subprocess.run(cmd.split(), input=args[1], encoding='utf-8', stderr=subprocess.STDOUT, timeout=None)
+except subprocess.CalledProcessError as e:
+    print('ERROR:', e.stdout)
+    exit()
+
+print('------------------compile done 2------------------')
+
+from exe_cython_c import main
+main()
+
+#os.remove('exe_cython_cmp.cp38-win_amd64.pyd')
